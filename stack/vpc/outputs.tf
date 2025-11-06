065b3c24588ac1089e170a1bece4633015a2c8c9
coder-prathamesh23 <premnaik2611@gmail.com>
2025-11-06 23:55:59 +0000

    VPC Stack

--- stack/vpc/outputs.tf
+++ stack/vpc/outputs.tf
@@ -1,51 +1,39 @@
 output "vpc_id" {
+  value       = module.vpc.vpc_id
   description = "VPC ID"
-  value       = aws_vpc.this.id
 }
 
 output "vpc_cidr_block" {
+  value       = module.vpc.vpc_cidr_block
   description = "CIDR Range for VPC"
-  value       = aws_vpc.this.cidr_block
 }
 
-output "subnets" {
-  description = "Subnet objects"
-
-  value = {
-    public  = aws_subnet.public
-    private = aws_subnet.private
-  }
+output "private_subnet_ids" {
+  value       = module.vpc.private_subnets
+  description = "List of private subnets IDs from VPC module"
 }
 
-output "subnet_ids" {
-  description = "Subnet IDs, public, private"
-  value = {
-    public  = [for subnet in aws_subnet.public : subnet.id]
-    private = [for subnet in aws_subnet.private : subnet.id]
-  }
-}
-
-output "public_subnets" {
-  description = "Public Subnets of the VPC"
-  value       = [for subnet in aws_subnet.public : subnet.id]
-}
-
-output "private_subnets" {
-  description = "Private Subnets of the VPC"
-  value       = [for subnet in aws_subnet.private : subnet.id]
+output "public_subnet_ids" {
+  value       = module.vpc.public_subnets
+  description = "List of public subnet IDs from VPC module"
 }
 
 output "public_route_table_id" {
-  description = "Public subnet route table ID for the VPC"
-  value       = aws_route_table.public.id
+  value       = module.vpc.public_route_table_id
+  description = "Returns public route table id"
 }
 
-output "private_route_table_id" {
-  description = "List of Private subnet route table IDs of VPC"
-  value       = [for table in aws_route_table.private : table.id]
+output "private_route_table_ids" {
+  value       = module.vpc.private_route_table_ids
+  description = "List of private route table ids"
+}
+
+output "shared_db_security_group_id" {
+  value       = aws_security_group.shared_db.id
+  description = "Shared DB security group for this VPC"
 }
 
 output "public_nat_cidr_blocks" {
+  value       = module.vpc.public_nat_cidr_blocks
   description = "Public NAT Gateway IP CIDR Ranges"
-  value       = [for nat in aws_eip.nat_gateway : "${nat.public_ip}/32"]
 }
